da629d912febfc007511deb13bcecf8899b7e2e5
Nice-PLQ <Nice-PLQ@users.noreply.github.com>
2023-06-04 04:13:26 +0000

    deploy: f4d1080ea06d37ca1975b6c0a5f2c565e125ae07

---
+++ assets/README-8cf2bfb2.js
@@ -0,0 +1,32 @@
+import{o as a,c as n,a2 as t}from"./index-5ac088ad.js";const e={class:"markdown-body"},r=t(`<h1>SearchBar</h1><hr><h4>1、基础用法</h4><p>通过<code>type</code>设置搜索框的样式</p><div class="code-wrapper"><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br></div><pre><code class="hljs html"><span class="hljs-tag">&lt;<span class="hljs-name">v-search-bar</span>
+  <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;capsule&quot;</span>
+  <span class="hljs-attr">v-model</span>=<span class="hljs-string">&quot;keyword&quot;</span>
+/&gt;</span>
+
+<span class="hljs-tag">&lt;<span class="hljs-name">v-search-bar</span>
+  <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;capsule&quot;</span>
+  <span class="hljs-attr">v-model</span>=<span class="hljs-string">&quot;keyword1&quot;</span>
+  <span class="hljs-attr">back-btn-visible</span>
+/&gt;</span>
+
+<span class="hljs-tag">&lt;<span class="hljs-name">v-search-bar</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;form&quot;</span> <span class="hljs-attr">v-model</span>=<span class="hljs-string">&quot;keyword2&quot;</span> /&gt;</span>
+</code></pre></div><div class="code-wrapper"><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br></div><pre><code class="hljs js"><span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> defineComponent({
+  setup() {
+    <span class="hljs-keyword">return</span> {
+      <span class="hljs-attr">keyword</span>: ref(<span class="hljs-string">&#39;&#39;</span>),
+      <span class="hljs-attr">keyword1</span>: ref(<span class="hljs-string">&#39;&#39;</span>),
+      <span class="hljs-attr">keyword2</span>: ref(<span class="hljs-string">&#39;&#39;</span>)
+    }
+  }
+})
+<span class="hljs-comment">// or</span>
+<span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  data() {
+    <span class="hljs-keyword">return</span> {
+      <span class="hljs-attr">keyword</span>: <span class="hljs-string">&quot;&quot;</span>,
+      <span class="hljs-attr">keyword1</span>: <span class="hljs-string">&quot;&quot;</span>,
+      <span class="hljs-attr">keyword2</span>: <span class="hljs-string">&quot;&quot;</span>
+    };
+  }
+};
+</code></pre></div><h2>Props</h2><table><thead><tr><th>属性</th><th>说明</th><th>类型</th><th>默认值</th><th>可选值</th></tr></thead><tbody><tr><td>v-model/value</td><td>输入框的值</td><td><em>string</em></td><td>-</td><td></td></tr><tr><td>type</td><td>搜索框样式</td><td><em>string</em></td><td><code>capsule</code></td><td><code>capsule</code>|<code>form</code></td></tr><tr><td>placeholder</td><td>提示信息</td><td><em>string</em></td><td><code>搜索</code></td><td></td></tr><tr><td>back-btn-visible</td><td>左侧后退箭头是否显示</td><td><em>boolean</em></td><td><code>false</code></td><td></td></tr><tr><td>cancel-btn-visible</td><td>右侧取消按钮是否显示</td><td><em>boolean</em></td><td><code>false</code></td><td></td></tr></tbody></table><h2>Event</h2><table><thead><tr><th>事件名</th><th>说明</th><th>参数</th></tr></thead><tbody><tr><td>back-btn-click</td><td>点击左侧后退箭头时触发</td><td>-</td></tr><tr><td>change</td><td>搜索框文字内容变化时触发</td><td><code>(value: String)</code></td></tr><tr><td>search</td><td>点击键盘的回车键时触发</td><td><code>(value: String)</code></td></tr><tr><td>focus</td><td>搜索框聚焦时触发</td><td>-</td></tr><tr><td>blur</td><td>搜索框失去焦点时触发</td><td>-</td></tr><tr><td>clear</td><td>清除搜索框内容时触发</td><td>-</td></tr><tr><td>cancel</td><td>点击取消按钮时触发</td><td>-</td></tr></tbody></table>`,10),l=[r],h={__name:"README",setup(p,{expose:s}){return s({frontmatter:{}}),(c,o)=>(a(),n("div",e,l))}};export{h as default};
---
+++ assets/contribute-84dec6e0.js
@@ -0,0 +1,23 @@
+import{o as l,c as s,a2 as i}from"./index-5ac088ad.js";const t={class:"markdown-body"},o=i(`<h1>开发指南</h1><h3>介绍</h3><p>首先感谢你关注 Vion 组件库，并进行代码贡献。 以下是关于向 Vion 提交代码的指南。在向 Vion 提交 Issue 或者 MR 之前，请先花几分钟时间阅读以下文字</p><h3>Issue 规范</h3><ul><li>遇到问题时，请先确认这个问题已经在 issue 中有记录或者已被修复</li><li>提 Issue 时，请用简短的语言描述遇到的问题，并添加出现问题时的环境和复现步骤</li></ul><h3>Commit Message格式规范</h3><ul><li>feat: 新特性</li><li>fix: 修改问题</li><li>refactor: 代码重构</li><li>docs: 文档修改</li><li>style: 代码格式修改, 注意不是 css 修改</li><li>test: 测试用例修改</li><li>chore: 其他修改, 比如构建流程, 依赖管理</li></ul><h3>Merge Request 规范</h3><ul><li>如果遇到问题，建议保持你的 MR 足够小。保证一个 MR 只解决一个问题或只添加一个功能</li><li>当新增组件或者修改原有组件时，记得增加或者修改测试代码，保证代码的稳定</li><li>在 MR 中请添加合适的描述，并关联相关的 Issue</li></ul><h3>Merge Request 流程</h3><ol><li>从主仓库中Fork一份到自己的代码仓库</li><li>新建开发分支，分支规范要求如下： <ul><li>修复组件 bug，分支命名形式为 <strong>bugfix/[组件名]_[问题简称]</strong> ，如：<code>bugfix/button_color</code></li><li>新增全新组件，分支命名形式为 <strong>feature/[组件名]</strong>，如：<code>feature/button</code></li><li>新增组件特性，分支命名形式为 <strong>feature/[组件名]_[功能]</strong>，如：<code>feature/button_loading</code></li></ul></li><li>在新分支上开发完成后，提 Merge Request 到仓库的 dev 分支</li><li>Merge Request 会在 Review 通过后被合并到仓库 Master 分支</li></ol><h3>目录结构</h3><ul><li>仓库的组件代码位于 src 下，每个组件一个文件夹</li><li>site 目录下是文档网站的代码，本地查看网站效果时可以在目录下运行 <code>npm run dev</code> 开启文档网站</li></ul><p>项目目录大致如下：</p><pre class="hljs"><code>├─ script    # 构建脚本
+├─ lib       # 产物
+├─ es        # ESM产物
+├─ docs      # 文档
+├─ src       # 组件
+├─ site      # 网站
+├─ types     # 类型定义
+</code></pre><h3>添加新组件</h3><p>添加新组件时，请按照下面的目录结构组织文件，并在 site/[demo|docs]/config.js 中配置组件名称及相关信息。运行<code>npm run dev</code>即可以打开浏览器查看组件示例了。</p><pre class="hljs"><code>src
+|- button
+|  ├─ __test__         # 单元测试
+|  ├─ demo             # 示例代码
+|  ├─ button.vue       # 组件源码
+|  ├─ index.js         # 组件入口
+|  ├─ style.js         # 样式入口
+|  ├─ README.md        # 组件文档
+|
+|- style
+|  ├─ button.less      # 组件样式
+|
+├─ index.js            # 所有组件入口
+├─ index-es.js         # ESM组件入口
+├─ config.js           # 配置文件
+</code></pre><h3>关于相关命名规范</h3><ul><li>样式的 class 必须以<code>v-</code>作为前缀，并遵循<a href="https://github.com/Tencent/tmt-workflow/wiki/%E2%92%9B-%5B%E8%A7%84%E8%8C%83%5D--CSS-BEM-%E4%B9%A6%E5%86%99%E8%A7%84%E8%8C%83">BEM</a>命名规范</li><li>组件 API 命名尽可能语义简洁，不能带有业务含义</li></ul>`,20),n=[o],h={__name:"contribute",setup(c,{expose:e}){return e({frontmatter:{}}),(u,d)=>(l(),s("div",t,n))}};export{h as default};
