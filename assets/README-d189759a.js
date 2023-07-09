4147d909b0d9068ae666c71f4e92e5023e93cf53
Nice-PLQ <Nice-PLQ@users.noreply.github.com>
2023-07-09 13:55:13 +0000

    deploy: 23b8a973f6d08593bcfe58fa775feb47294f6bca

---
+++ assets/README-d189759a.js
@@ -0,0 +1,21 @@
+import{o as a,c as n,a2 as t}from"./index-2a7e0456.js";const e={class:"markdown-body"},l=t(`<h1>Toast</h1><hr><h4>1、基础用法</h4><p>Toast 组件默认在 vue 实例下挂载<code>$toast</code>对象，方便在业务代码中快捷调用</p><div class="code-wrapper"><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br></div><pre><code class="hljs html"><span class="hljs-tag">&lt;<span class="hljs-name">v-button</span> @<span class="hljs-attr">click</span>=<span class="hljs-string">&quot;success&quot;</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;primary&quot;</span>&gt;</span>完成类提示<span class="hljs-tag">&lt;/<span class="hljs-name">v-button</span>&gt;</span>
+<span class="hljs-tag">&lt;<span class="hljs-name">v-button</span> @<span class="hljs-attr">click</span>=<span class="hljs-string">&quot;error&quot;</span> <span class="hljs-attr">type</span>=<span class="hljs-string">&quot;warning&quot;</span>&gt;</span>警示类提示<span class="hljs-tag">&lt;/<span class="hljs-name">v-button</span>&gt;</span>
+<span class="hljs-tag">&lt;<span class="hljs-name">v-button</span> @<span class="hljs-attr">click</span>=<span class="hljs-string">&quot;info&quot;</span>&gt;</span>通知类提示<span class="hljs-tag">&lt;/<span class="hljs-name">v-button</span>&gt;</span>
+</code></pre></div><div class="code-wrapper"><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br></div><pre><code class="hljs js"><span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
+  <span class="hljs-attr">methods</span>: {
+    success() {
+      <span class="hljs-keyword">this</span>.$toast.success(<span class="hljs-string">&quot;完成类提示&quot;</span>);
+    },
+    error() {
+      <span class="hljs-keyword">this</span>.$toast.error(<span class="hljs-string">&quot;警示类提示&quot;</span>);
+    },
+    info() {
+      <span class="hljs-keyword">this</span>.$toast({
+        <span class="hljs-attr">type</span>: <span class="hljs-string">&quot;info&quot;</span>,
+        <span class="hljs-attr">message</span>: <span class="hljs-string">&quot;通知类提示&quot;</span>,
+        <span class="hljs-attr">duration</span>: <span class="hljs-number">3000</span>, <span class="hljs-comment">// 3s后隐藏</span>
+      });
+    },
+  },
+};
+</code></pre></div><h2>Props</h2><h4>快捷调用</h4><table><thead><tr><th>方法名</th><th>说明</th><th>参数</th></tr></thead><tbody><tr><td>Toast.success</td><td>成功提示</td><td><code>options</code>|<code>message</code></td></tr><tr><td>Toast.error</td><td>错误提示</td><td><code>options</code>|<code>message</code></td></tr><tr><td>Toast.info</td><td>信息提示</td><td><code>options</code>|<code>message</code></td></tr></tbody></table><h5>options</h5><table><thead><tr><th>参数</th><th>说明</th><th>类型</th><th>可选值</th></tr></thead><tbody><tr><td>type</td><td>toast 类型</td><td><em>string</em></td><td><code>success</code>|<code>error</code>|<code>info</code></td></tr><tr><td>message</td><td>提示文案</td><td><em>string</em></td><td>-</td></tr><tr><td>duration</td><td>自动隐藏时长（单位 ms）</td><td><em>number</em></td><td><code>1500</code></td></tr><tr><td>safe</td><td>是否适配刘海屏</td><td><em>boolean</em></td><td><code>false</code></td></tr></tbody></table>`,11),p=[l],i={__name:"README",setup(r,{expose:s}){return s({frontmatter:{}}),(o,d)=>(a(),n("div",e,p))}};export{i as default};
---
+++ assets/i18n-7699dfc0.js
@@ -0,0 +1,27 @@
+import{o as n,c as a,a2 as e}from"./index-2a7e0456.js";const p={class:"markdown-body"},l=e(`<h1>国际化</h1><p>Vion 默认采用中文语言，如果需要切换其他语言，请参考如下</p><h3>1、切换语言</h3><p>Vion 通过 Locale 组件实现多语言支持，使用<code>Locale.use</code>方法切换语言环境，比如切换到英文环境</p><div class="code-wrapper"><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br></div><pre><code class="hljs js"><span class="hljs-comment">// 按需引入</span>
+<span class="hljs-keyword">import</span> { createApp } <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;vue&quot;</span>;
+<span class="hljs-keyword">import</span> { ActionSheet, Locale } <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;vion&quot;</span>;
+<span class="hljs-keyword">import</span> messages <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;vion/es/locale/lang/en-US&quot;</span>;
+
+Locale.use(<span class="hljs-string">&quot;en-US&quot;</span>, messages);
+
+createApp()
+  .use(ActionSheet);
+  .use(Locale);
+
+</code></pre></div><p>或</p><div class="code-wrapper"><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br></div><pre><code class="hljs js"><span class="hljs-comment">// 全量引入Vion</span>
+<span class="hljs-keyword">import</span> { createApp } <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;vue&quot;</span>;
+<span class="hljs-keyword">import</span> Vion <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;vion&quot;</span>;
+<span class="hljs-keyword">import</span> messages <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;vion/es/locale/lang/en-US&quot;</span>;
+
+vion.Locale.use(<span class="hljs-string">&quot;en-US&quot;</span>, messages);
+
+createApp().use(Vion);
+</code></pre></div><h3>2、语言包</h3><p>目前 vion 内置了以下几种语言</p><ul><li>简体中文（zh-CN）</li><li>繁体中文（zh-Hant）</li><li>英语（en-US）</li><li>日语（ja-JP）</li></ul><p>如果不能满足业务场景，只需要按照<a href="https://github.com/Nice-PLQ/vion/blob/master/src/locale/lang/zh-CN.js">语言包格式</a>自定义语言，然后使用<code>Locale</code>组件加载即可，如：</p><div class="code-wrapper"><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br></div><pre><code class="hljs js"><span class="hljs-keyword">import</span> messages <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;./lang/fr-FR&quot;</span>; <span class="hljs-comment">// 自定义的【法语】语言包</span>
+Locale.use(<span class="hljs-string">&quot;fr-FR&quot;</span>, messages);
+</code></pre></div><h3>3、引入多个语言包</h3><div class="code-wrapper"><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br></div><pre><code class="hljs js"><span class="hljs-keyword">import</span> frMessages <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;./lang/fr-FR&quot;</span>; <span class="hljs-comment">// 自定义的【法语】语言包</span>
+<span class="hljs-keyword">import</span> trMessages <span class="hljs-keyword">from</span> <span class="hljs-string">&quot;./lang/tr-TR&quot;</span>; <span class="hljs-comment">// 自定义的【土耳其语】语言包</span>
+
+Locale.add(<span class="hljs-string">&quot;tr-TR&quot;</span>, trMessages); <span class="hljs-comment">// 将语言包加入到集合中</span>
+Locale.use(<span class="hljs-string">&quot;fr-FR&quot;</span>, frMessages); <span class="hljs-comment">// 默认使用法语作为首选语言</span>
+</code></pre></div>`,14),r=[l],u={__name:"i18n",setup(c,{expose:s}){return s({frontmatter:{}}),(i,t)=>(n(),a("div",p,r))}};export{u as default};
